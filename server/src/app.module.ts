af387cfe8025fd508cbaf8b722ca74e156ede785
timo <tmhermans@gmail.com>
2020-01-27 07:33:41 +0100

    Remove unused app controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { QuotesModule } from './quotes/quotes.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [QuotesModule, ConfigModule.forRoot({ isGlobal: true })],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
