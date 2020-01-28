635e7080adc0db0c7425febd5cfc852cfb725388
timo <tmhermans@gmail.com>
2020-01-28 10:31:33 +0100

    #6 Make it possible to retrieve a new quote

--- client/src/app/features/quotes/pages/quote-random/quote-random.component.spec.ts
+++ client/src/app/features/quotes/pages/quote-random/quote-random.component.spec.ts
@@ -5,6 +5,7 @@ import { Quote } from '../../models/quote.model';
 import * as faker from 'faker';
 import { of } from 'rxjs';
 import { IsLoadingService } from '@service-work/is-loading';
+import { take } from 'rxjs/operators';
 
 jest.mock('../../services/quote.service');
 jest.mock('@service-work/is-loading');
@@ -14,6 +15,9 @@ describe('QuoteRandomComponent', () => {
   let service: jest.Mocked<QuoteService>;
   let loadingService: jest.Mocked<IsLoadingService>;
 
+  const createRandomQuote = () =>
+    new Quote(faker.name.firstName(), faker.random.words(5));
+
   beforeEach(() => {
     service = new QuoteService(null) as jest.Mocked<QuoteService>;
     loadingService = new IsLoadingService() as jest.Mocked<IsLoadingService>;
@@ -22,10 +26,8 @@ describe('QuoteRandomComponent', () => {
 
   it('fetches a random quote on page start', (done: DoneCallback) => {
     // arrange
-    const expectedQuote = new Quote(
-      faker.name.firstName(),
-      faker.random.words(5)
-    );
+    const expectedQuote = createRandomQuote();
+
     service.getRandom.mockReturnValue(of(expectedQuote));
 
     // act
@@ -34,6 +36,26 @@ describe('QuoteRandomComponent', () => {
     // assert
     component.quote$.subscribe((quote: Quote) => {
       expect(quote).toEqual(expectedQuote);
+      done();
+    });
+  });
+
+  it('fetches a new quote when user asks for it', (done: DoneCallback) => {
+    // arrange
+    const expectedFirstQuote = createRandomQuote();
+    const expectedSecondQuote = createRandomQuote();
+    service.getRandom
+      .mockReturnValueOnce(of(expectedFirstQuote))
+      .mockReturnValueOnce(of(expectedSecondQuote));
+
+    component = new QuoteRandomComponent(service, loadingService);
+
+    // act
+    component.getNewRandomQuote();
+
+    // assert
+    component.quote$.pipe(take(1)).subscribe((quote: Quote) => {
+      expect(quote).toEqual(expectedSecondQuote);
       done();
     });
   });
