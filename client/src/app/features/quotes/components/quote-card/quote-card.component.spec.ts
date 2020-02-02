0b2bd7caacd1c1b4e862888bb7fd1a36bf5498a5
timo <tmhermans@gmail.com>
2020-02-02 14:10:26 +0100

    Add tests for the quote card component

--- client/src/app/features/quotes/components/quote-card/quote-card.component.spec.ts
+++ client/src/app/features/quotes/components/quote-card/quote-card.component.spec.ts
@@ -25,4 +25,33 @@ describe('QuoteCardComponent', () => {
     component.rate.pipe(take(1)).subscribe(assertFn);
     component.rateQuote(5);
   });
+
+  it('does not show rating results when a quote does not have them', () => {
+    const component = new QuoteCardComponent();
+
+    component.quote = createRandomQuote();
+
+    expect(component.areResultsVisible).toBeFalsy();
+  });
+
+  it('is not possible to rate a quote when there is none', () => {
+    const component = new QuoteCardComponent();
+    jest.spyOn(component.rate, 'emit');
+
+    component.rateQuote(5);
+
+    expect(component.quote).toBeFalsy();
+    expect(component.rate.emit).not.toHaveBeenCalled();
+  });
+
+  it('shows rating result when there are results to show', () => {
+    const component = new QuoteCardComponent();
+
+    const quote = createRandomQuote();
+    quote.ratingAverage = 3.7;
+    quote.amountOfVotes = 15;
+    component.quote = quote;
+
+    expect(component.areResultsVisible).toBeTruthy();
+  });
 });
